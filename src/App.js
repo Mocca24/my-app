72d6dad663cf8cea24eeb62277f81cf47c709210
Mocca24 <signupcyn@gmail.com>
2023-09-25 12:46:21 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+              <footer>
+          This project was coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/cynthia-alvez/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Cynthia Alvez
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/Mocca24/my-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://graceful-griffin-d70005.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hosted on Netlify
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
-
-export default App;
