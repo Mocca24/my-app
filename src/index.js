5aca22bd96d5e0ba05f5e8022b27fb69eaffeb04
Mocca24 <signupcyn@gmail.com>
2023-09-25 15:40:00 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./Weather"
 import "./index.css";
+import "./Weather.css"
 import App from "./App";
 
 const rootElement = document.getElementById("root");
