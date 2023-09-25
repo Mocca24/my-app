4fe9391fad439db3ff7546584d826695e7829ff9
Mocca24 <signupcyn@gmail.com>
2023-09-25 15:13:37 +0300

    Update Weather js

--- src/Weather.js
+++ src/Weather.js
@@ -3,6 +3,20 @@ import "./Weather.css";
 
 export default function Weather () {
     return (
-        <div className="Weather">hi</div>
+        <div className="Weather">
+        <h1>New York</h1>
+        <ul>
+            <li>Monday 08:00</li>
+            <li>Light rain showers</li>
+        </ul>
+        <div className="row">
+            <div className="col-6">
+                <img src= "https://ssl.gstatic.com/onebox/weather/64/rain_light.png"
+                alt="Light rain showers">
+
+                </img>
+        </div> 
+        </div>
+        </div>
     )
 }
