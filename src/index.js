828adcf97b5840d6b185cab1f8e754a5201ea739
Mocca24 <signupcyn@gmail.com>
2023-09-28 10:18:26 +0300

    removed bootstrap

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import { createRoot } from "react-dom/client";
 import "./Weather"
 import "./Weather.css";
 import App from "./App";
-import "bootstrap/dist/css/bootstrap.css";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
