37021868ced98c4415fca5ae29da32ea6dab923c
Mocca24 <signupcyn@gmail.com>
2023-09-28 10:07:01 +0300

    api connection

--- src/Weather.js
+++ src/Weather.js
@@ -1,8 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Weather.css";
+import axios from "axios";
 
-export default function Weather () {
-    return (
+export default function Weather (props) {
+
+    const [weatherData, setWeatherData] = useState({ready: false});
+    const [temperature, setTemperature]=useState(null);
+    function handleResponse(response) {
+        console.log(response.data);
+        setWeatherData({
+            ready: true,
+            temperature: response.data.main.temp,
+            humidity: response.data.main.humidity,
+            date:"Monday 08:00",
+            description:response.data.weather[0].description,
+            iconUrl:"https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
+            wind: response.data.wind.speed,
+            city: response.data.main.name,
+        });
+
+        setTemperature(response.data.main.temp)
+    }
+
+if (weatherData.ready) {
+      return (
         <div className="Weather">
         <form>
             <div className="row">
@@ -20,30 +41,44 @@ export default function Weather () {
             </div>
             </div>
         </form>
-        <h1>New York</h1>
+
+        <h1>{weatherData.city}</h1>
+
         <ul>
-            <li>Monday 08:00</li>
-            <li>Light rain showers</li>
+            <li>{weatherData.date}</li>
+            <li clossName="text-capitalize">{weatherData.description}</li>
         </ul>
+
         <div className="row mt-3">
             <div className="col-6">
-                <img src= "https://ssl.gstatic.com/onebox/weather/64/rain_light.png"
-                alt="Light rain showers"
+                <img src= {weatherData.iconUrl}
+                alt={weatherData.description} 
                 className="float-left"
                 />
             <div className="float-left">
-            <span className="temperature">6</span>
+            <span className="temperature">{Math.round(temperature)}</span>
             <span className="unit">°C</span>
             </div>
+            </div>
         </div>
+
         <div className="col-6">
             <ul>
                 <li> Precipitation: 46% </li>
-                <li> Humidity: 98% </li>
-                <li> Wind: 18 km/h </li>
+                <li> Humidity: {weatherData.humidity} </li>
+                <li> Wind: {weatherData.wind} km/h </li>
             </ul>
         </div>
+
         </div>
-        </div>
-    )
+    );
+    }else{
+    const apiKey="311f1f45fee82242ab4086372ab360f5";
+    let city="London"
+    `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+
+    return "Loading.."
+    }
+  
 }
