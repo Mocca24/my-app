9981ec41acb34f64cbc8670c30bbc017c98bc199
Mocca24 <signupcyn@gmail.com>
2023-09-30 18:04:44 +0300

    bootstrap

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,4 +1,5 @@
 import React, {useState} from "react";
+import "bootstrap/dist/css/bootstrap.css";
 
 export default function WeatherTemperature(props) {
     const [unit, setUnit] = useState("celsius");
