fa3bba3ff01fc76909a5c3f73e1ccc75454aa3e9
Mocca24 <signupcyn@gmail.com>
2023-10-03 15:00:33 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import { createRoot } from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.css";
 
 import "./index.css";
-import "bootstrap/dist/css/bootstrap.css";
 import App from "./App";
+import "./App.css"
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
